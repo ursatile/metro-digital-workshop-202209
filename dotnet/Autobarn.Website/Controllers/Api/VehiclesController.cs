012e39bd7703458a726d5c40aec407701d49032f
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-05 11:59:25 +0300

    HTTP PUT with status codes

--- dotnet/Autobarn.Website/Controllers/Api/VehiclesController.cs
+++ dotnet/Autobarn.Website/Controllers/Api/VehiclesController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using Autobarn.Data;
 using Autobarn.Data.Entities;
+using Autobarn.Website.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Migrations.Operations;
 
 namespace Autobarn.Website.Controllers.Api {
     [Route("api/[controller]")]
@@ -31,16 +33,31 @@ namespace Autobarn.Website.Controllers.Api {
         //public void Post([FromBody] string value) {
         //}
 
-        //// PUT api/<VehiclesController>/5
-        //[HttpPut("{id}")]
-        //public void Put(int id, [FromBody] string value) {
-        //}
+        // PUT api/<VehiclesController>/5
+        [HttpPut("{id}")]
+        public IActionResult Put(string id, [FromBody] VehicleDto dto) {
+            var model = db.FindModel(dto.ModelCode);
+            if (model == null) return BadRequest("we couldn't find that model");
+            if (dto.Registration != id) return BadRequest("Registration plate must match URL");
+            var vehicle = new Vehicle() {
+                VehicleModel = model,
+                Color = dto.Color,
+                Year = dto.Year,
+                Registration = id
+            };
+            db.CreateVehicle(vehicle);
+            return Ok(vehicle);
+        }
 
         //// DELETE api/vehicles/{id}
         [HttpDelete("{id}")]
-        public void Delete(int id) {
+        public IActionResult Delete(string id) {
             /* todo: delete the vehicle with the specified id */
             /* question: what should we return here? */
+            var vehicle = db.FindVehicle(id);
+            if (vehicle == null) return NotFound();
+            db.DeleteVehicle(vehicle);
+            return Ok();
         }
     }
 }
