0be688aaee24f9bee6edb615625884d6e6893fae
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-06 15:12:06 +0300

    HostedService setup for PricingClient

--- dotnet/Autobarn.PricingClient/Program.cs
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -1,12 +1,31 @@
 ﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
-namespace Autobarn.PricingClient
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+namespace Autobarn.PricingClient {
+    internal class Program {
+        static async Task Main(string[] args) {
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((hostContext, services) => {
+                    services.AddHostedService<PricingClientWorker>();
+                })
+                .Build();
+            await host.RunAsync();
         }
     }
+
+    internal class PricingClientWorker : IHostedService {
+        public Task StartAsync(CancellationToken cancellationToken) {
+            Console.WriteLine("Started the pricing client!");
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken) {
+            Console.WriteLine("Stopping the pricing client!");
+            return Task.CompletedTask;
+        }
+    }
+
 }
