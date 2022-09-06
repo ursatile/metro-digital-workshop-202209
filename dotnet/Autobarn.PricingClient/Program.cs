68468f0437941d75fe2d16bcd40b7990691c0903
Dylan Beattoie <dylan@dylanbeattie.net>
2022-09-06 15:19:08 +0300

    Logging added to Pricingclient

--- dotnet/Autobarn.PricingClient/Program.cs
+++ dotnet/Autobarn.PricingClient/Program.cs
@@ -3,6 +3,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 namespace Autobarn.PricingClient {
     internal class Program {
@@ -17,13 +18,24 @@ namespace Autobarn.PricingClient {
     }
 
     internal class PricingClientWorker : IHostedService {
+        private readonly ILogger<PricingClientWorker> logger;
+
+        public PricingClientWorker(ILogger<PricingClientWorker> logger) {
+            this.logger = logger;
+        }
         public Task StartAsync(CancellationToken cancellationToken) {
-            Console.WriteLine("Started the pricing client!");
+            logger.LogCritical("THIS IS A CRITICAL PROBLEM");
+            logger.LogError("This went wrong but somebody probably tried again and it worked!");
+            logger.LogWarning("Something went wrong but nobody noticed");
+            logger.LogInformation("Started the pricing client!");
+            logger.LogDebug("This is really detailed debugging information");
+            logger.LogTrace("This is even more detailed debugging information");
             return Task.CompletedTask;
+
         }
 
         public Task StopAsync(CancellationToken cancellationToken) {
-            Console.WriteLine("Stopping the pricing client!");
+            logger.LogInformation("Stopping the pricing client!");
             return Task.CompletedTask;
         }
     }
